988cd1fa89fbd034a9d5822b5004d12cc62816f9
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-10-17 04:34:04 +0900

    feat: CRUDの動作確認コードを追加

---
+++ backend/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import os
+
+SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
