d74a8dbaa46ec67de12d12d211893ba24fed418d
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-10-17 04:20:27 +0900

    feat: トップページを追加

---
+++ frontend/pages/index.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/header";
+import Layout from "../components/layout";
+
+const Top = () => {
+  return (
+    <Layout>
+      <Header />
+      <p>TOP PAGE</p>
+    </Layout>
+  )
+};
+
+export default Top;
