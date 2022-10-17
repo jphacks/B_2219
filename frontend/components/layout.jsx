2c8406ad28368b29352504be8faa582833ee7752
kakudo <31888089+kakudo415@users.noreply.github.com>
2022-10-17 19:35:39 +0900

    fix: crossoriginをcamelCaseに

--- frontend/components/layout.jsx
+++ frontend/components/layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" rel="stylesheet" />
       </Head>
       <div className={styles.container}>{children}</div>
