4f2adedd1c1f6be39ef3be85b307e4cf4ea8f7b2
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-10-22 07:36:38 +0900

    refactor: 不要行を削除 (#13)

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,40 +1,4 @@
-from email import message
-import string
 from pydantic import BaseModel
-
-
-class ItemBase(BaseModel):
-    title: str
-    description: str | None = None
-
-
-class ItemCreate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
-    id: int
-    owner_id: int
-
-    class Config:
-        orm_mode = True
-
-
-class UserBase(BaseModel):
-    email: str
-
-
-class UserCreate(UserBase):
-    password: str
-
-
-class User(UserBase):
-    id: int
-    is_active: bool
-    items: list[Item] = []
-
-    class Config:
-        orm_mode = True
 
 
 class ProjectCreate(BaseModel):
@@ -62,15 +26,6 @@ class FileCreate(BaseModel):
 
 class File(FileCreate):
     id: int
-
-    class Config:
-        orm_mode = True
-
-
-class Commit(BaseModel):
-    id: str
-    message: str
-    files: list[File] = []
 
     class Config:
         orm_mode = True
